9884a1300f803385bdb7697883a4db3944389ab6
boofmang <topmcveagh@gmail.com>
2024-12-05 13:26:39 +0000

    Added Human Choice input!

--- java.js
+++ java.js
@@ -1,3 +1,28 @@
+
+
+function getHumanChoice() {
+    let answer = prompt("Choose your destiny! Rock / Paper / Scissors ");
+    
+    if (answer.toLowerCase() === "rock") {
+        return("You choose Rock!");
+    }
+    else if (answer.toLowerCase() === "paper") {
+        return("You choose Paper!");
+    }
+    else if (answer.toLowerCase() === "scissors")
+     {
+        return("You choose Scissors!") }
+        else { return("One of three dumbshit!")}
+     }
+
+    
+
+
+
+
+
+
+
 function getComputerChoice() {
     let bot = Math.floor(Math.random() * 3)
     if (bot === 0) {
