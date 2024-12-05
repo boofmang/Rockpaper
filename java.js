1b2b0b5f9326f5edf4527ec2d97091df838b7111
boofmang <topmcveagh@gmail.com>
2024-12-05 16:28:25 +0000

    Made the basics for the round play score counter works

--- java.js
+++ java.js
@@ -1,41 +1,40 @@
 
-
-function getHumanChoice() {
-    let answer = prompt("Choose your destiny! Rock / Paper / Scissors ");
-    
-    if (answer.toLowerCase() === "rock") {
-        return("You choose Rock!");
-    }
-    else if (answer.toLowerCase() === "paper") {
-        return("You choose Paper!");
-    }
-    else if (answer.toLowerCase() === "scissors")
-     {
-        return("You choose Scissors!") }
-        else { return("One of three dumbshit!")}
-     }
-
-    
+let humanscore = 0
+let botscore = 0
 
 
-
-
-
-
-
-function getComputerChoice() {
-    let bot = Math.floor(Math.random() * 3)
-    if (bot === 0) {
-        return "Paper";
-    }
-    else if (bot === 1) {
-        return "Rock"
-    }
-    else {
-        return "Scissors"
-    }
-
-
+    function getComputerChoice() {
+        let choices = ["rock", "paper", "scissors"];
+    let bot = choices[Math.floor(Math.random() * 3)];
+    return bot
 
 }
 
+
+function playRound() {
+    const humanchoice = prompt("Pick one bitch! Rock, Paper or Scissors?").toLowerCase();
+    const botchoice = getComputerChoice();
+    if (humanchoice === botchoice) 
+    {
+        console.log("Power is evenly matched!")
+    }
+
+    else if 
+        (humanchoice === "rock" && botchoice === "scissors" ||
+            humanchoice === "scissors" && botchoice === "paper" ||
+            humanchoice === "paper" && botchoice === "rock") {
+            console.log("you smashed them!") 
+            humanscore ++; }
+
+            else if 
+            (botchoice === "rock" && humanchoice === "scissors" ||
+                botchoice === "scissors" && humanchoice === "paper" ||
+                botchoice === "paper" && humanchoice === "rock") {
+                console.log("get fucked on!")
+                botscore ++; }
+        }
+    
+    
+
+
+playRound()
